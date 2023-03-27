96d8510a2070bbaefe5ba6f93ca4445ba8f09b3e
greedybrute <98757280+greedybrute@users.noreply.github.com>
2023-03-28 00:13:58 +0530

    cors

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const io = require("socket.io")(server, {
   },
 });
 
-app.use(cors({ credentials: true }));
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+   next();
+}) 
 
 const PORT = process.env.PORT || 7777;
 
